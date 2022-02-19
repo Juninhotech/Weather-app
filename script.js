fc04a5f680f770aed4f3ba2747021d86f1d2d733
Juninho <juninhopeter1@gmail.com>
2022-02-19 11:46:11 -0800

    work on the javascript section

--- script.js
+++ script.js
@@ -1,15 +1,103 @@
 const wrapper = document.querySelector(".wrapper"),
 inputPart = wrapper.querySelector(".input-part"),
 infoTxt = inputPart.querySelector(".info-text"),
-inputField = inputPart.querySelector("input");
+inputField = inputPart.querySelector("input"),
+locationBtn = inputPart.querySelector("button"),
+wIcon = wrapper.querySelector(".weather-section img"),
+arrowKey = wrapper.querySelector("header i")
+
+let api;
 
 inputField.addEventListener("keyup", e =>{
-    if(e.key == "Enter" && inputField.value != ""){
+    if(e.key == "Enter" && inputField.value != ""){ 
         requestApi(inputField.value);
+    }
+});
+
+locationBtn.addEventListener("click", ()=>{
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
+    }else{
+        alert("Your browser not support geolocation api")
     }
 })
 
-function requestApi(city){
-    
+function onSuccess(position){
+    const {latitude, longitude} = position.coords
+    api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${"0d91a78baef4a350cdd9aea4a92bbcc0"}`;
+    fetchData();
+
+}
+function onError(error){
+    infoTxt.innerText = error.message
+    infoTxt.classList.add("error");
 }
 
+
+function requestApi(city){
+    api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${"0d91a78baef4a350cdd9aea4a92bbcc0"}`;
+    fetchData();
+}
+
+function fetchData(){
+    infoTxt.innerText = "Getting weather details....";
+    infoTxt.classList.add("pending");
+    fetch(api).then(response => response.json()).then(result => weatherDetails(result));
+}
+
+function weatherDetails(info){
+    infoTxt.classList.replace("pending", "error");
+    if(info.cod =='404'){
+        infoTxt.innerText = `${inputField.value} isn't a valid city name`;
+    }else{
+        const city = info.name;
+        const country = info.sys.country;
+        const {description, id} = info.weather[0];
+        const {feels_like, humidity, temp} = info.main;
+
+        if(id ==800){
+            wIcon.src = "clear.svg";
+        }else if(id >= 200 && id <= 232){
+            wIcon.src = "storm.svg";
+        }else if(id >= 600 && id<= 622){
+            wIcon.src = "snow.svg";
+        }else if(id >= 701 && id <= 781){
+            wIcon.src = "haze.svg";
+        }else if(id >= 801 && id <= 804){
+            wIcon.src = "cloud.svg";
+        }else if(id >= 300 && id <= 321){
+            wIcon.src = "rain.svg"
+        }else if(id >= 500 && id <= 531){
+            wIcon.src = "rain.svg";
+        }
+
+        wrapper.querySelector(".temp .numb").innerText = Math.floor(temp);
+        wrapper.querySelector(".weather").innerText = description;
+        wrapper.querySelector(".location span").innerText = `${city}, ${country}`;
+        wrapper.querySelector(".temp .numb-2").innerText = Math.floor(feels_like);
+        wrapper.querySelector(".humidity span").innerText = `${humidity}%`;
+
+
+
+        
+        // const city = info.name;
+        // const country = info.sys.country;
+        // const {description, id} = info.weather[0];
+        // const {feels_like, humidity, temp} = info.main;
+
+        // wrapper.querySelector(".temp .numb").innerText = Math.floor(temp);
+        // wrapper.querySelector(".weather").innerText = description;
+        // wrapper.querySelector(".location span").innerText = `${city}, ${country}`;
+        // wrapper.querySelector(".temp .numb-2").innerText = Math.floor(feels_like);
+        // wrapper.querySelector(".humidity, span").innerText = `${humidity}%`;
+
+        infoTxt.classList.remove("pending", "error");
+        wrapper.classList.add("active");
+    }
+}
+    
+
+arrowKey.addEventListener("click", ()=>{
+    wrapper.classList.remove("active");
+
+});
